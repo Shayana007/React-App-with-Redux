a05c6b32904273ca6a1f616b297331d33315f27d
Shayana007 <99860513+Shayana007@users.noreply.github.com>
2022-04-11 22:08:15 +0530

    Add files via upload

---
+++ reduxone.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Component1 extends Component{
+    render(){
+          return(
+              <div>
+                  <h1>functional component</h1>
+                  <h1>This is test component</h1>
+              </div>
+          );
+    }
+}
+export default Component1
