8f677aa767d3d2de0376a3232162f79ffbc8de3d
Shayana007 <99860513+Shayana007@users.noreply.github.com>
2022-04-11 22:10:12 +0530

    Add files via upload

---
+++ reduxForm.js
@@ -0,0 +1,27 @@
+import React, {Component} from "react";
+
+class Forms extends Component{
+    constructor(props)
+    {
+        super(props);
+        this.state={
+            data: 'enter name...'
+        }
+        this.updateState=this.updateState.bind(this);
+    };
+    updateState(e)
+    {
+        this.setState({data: e.target.value});
+
+    }
+    render(){
+        return(
+            <div className="App">
+                <input type="text" value={this.state.data} onChange={this.updateState}/>
+                <h2>{this.state.data}</h2>
+                <input type="password" value='test'/>
+            </div>
+        );
+    }
+}
+export default Forms
